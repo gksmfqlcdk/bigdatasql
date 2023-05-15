886c2567302db8dd8ea3eddf2749fd7015e2f9fe
gksmfqlcdk d <azaz4674@gmail.com>
2023-05-15 05:27:59 +0000

    가수이름으로 검색기능 추가

--- insert_data.py
+++ insert_data.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-# sqlite db 파일 생성
+# sqlite db 파일 생성 및 연결
 con = sqlite3.connect('dbdb.db')
 # sql 문장을 실행시키기 위해 준비
 cursor = con.cursor() 
@@ -11,4 +11,4 @@ VALUES (1, '이혜리', '1994-06-09')
 '''
 cursor.execute(sql) # sql 을 실행
 con.commit() # 적용
-con.close() # db닫기
+con.close()  # db닫기
